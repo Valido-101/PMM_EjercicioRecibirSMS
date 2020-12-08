33fd7305e893bdd0ffab435843da1f8b4669fcdf
Jesús Valido Zafra <jesuszafra22@gmail.com>
2020-12-08 14:08:50 +0100

    Éjercicio realizado con comentarios

--- app/src/main/java/com/example/ejercicio_sms/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_sms/MainActivity.java
@@ -21,6 +21,7 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Elementos necesarios para realizar el ejercicio
     ActivityResultLauncher<String> requestPermissionLauncher;
     private static MainActivity mainActivity_instance;
     Button btn_comprobar;
@@ -30,16 +31,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Aplicamos el layout correspondiente a la actividad
         setContentView(R.layout.activity_main);
 
+        //Obtenemos las vistas que necesitamos
         btn_comprobar = (Button)findViewById(R.id.btn_comprobar);
 
         text_codigo = (EditText)findViewById(R.id.text_codigo);
 
+        //Proporcionamos un OnClickListener al botón de comprobación
         btn_comprobar.setOnClickListener(this);
 
+        //Guardamos una instancia de esta actividad para poder referirnos a ella desde el BroadcastReceiver
         mainActivity_instance = this;
 
+        //Método necesario para pedir permisos al usuario en tiempo de ejecución
         requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>(){
             @Override
             public void onActivityResult(Boolean isGranted){
@@ -58,12 +64,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onStart() {
         super.onStart();
 
-        //Si ya se ha dado permiso procedemos con la descarga
+        //Si ya se ha dado permiso procedemos el desarrollo normal de la aplicación
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) == PackageManager.PERMISSION_GRANTED) {
 // You can use the API that requires the permission.
             //doSomeTaskAsync();
         }
-        //Si no se pide permiso y se hace la descarga
+        //Si no se pide permiso y se sigue el desarrollo normal de la aplicación
         else {
 // You can directly ask for the permission.
 // The registered ActivityResultCallback gets the result of this request.
@@ -71,19 +77,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //Método que nos permite referenciar a esta actividad desde el BroadcastReceiver
     public static MainActivity  devuelveInstancia(){
         return mainActivity_instance;
     }
 
+    //Método que sirve para actualizar el contenido del EditText
     public void actualizarEditText(final String t) {
+        //Ejecutamos este método en un hilo
         MainActivity.this.runOnUiThread(new Runnable() {
             public void run() {
+                //Obtenemos el EditText
                 EditText editText = (EditText) findViewById(R.id.text_codigo);
+                //Actualizamos el texto que contiene
                 editText.setText(t);
             }
         });
     }
 
+    //Método que nos permite almacenar el mensaje recibido para poder comprobar si es correcto
     public void setMensajeCodigo(String s)
     {
         mensaje_codigo=s;
@@ -91,26 +103,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        //Proceso para obtener el código del mensaje recibido
+
+        //Expresión regular necesaria para obtener el código
         String regex = "(\\d{8}$)";
 
+        //Patrón que buscará y encontrará el código en el texto del mensaje
         Pattern patron = Pattern.compile(regex);
 
+        //Matcher que buscará el patrón en el texto del mensaje, que guardamos previamente
         Matcher matcher = patron.matcher(mensaje_codigo);
 
         try {
+            //Encontramos el trozo de texto que coincide con el patrón
             matcher.find();
 
+            //Lo extraemos y comprobamos si es correcto
             if((text_codigo.getText().toString().equals(matcher.group(1))))
             {
+                //Si lo es informamos al usuario
                 Toast.makeText(this,"Código correcto",Toast.LENGTH_LONG).show();
             }
             else
                 {
+                    //Si no lo es, informamos al usuario
                     Toast.makeText(this,"Código incorrecto", Toast.LENGTH_LONG).show();
                 }
 
         }catch(IllegalStateException e)
         {
+            //Si no se ha encontrado el patrón, se informa al usuario
             Toast.makeText(this,"Formato de código inválido", Toast.LENGTH_LONG).show();
         }
     }
--- app/src/main/java/com/example/ejercicio_sms/SMS_Receiver.java
+++ app/src/main/java/com/example/ejercicio_sms/SMS_Receiver.java
@@ -21,38 +21,49 @@ public class SMS_Receiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        // Get the SMS message.
+        // Elementos necesarios para obtener el mensaje sms
         Bundle bundle = intent.getExtras();
         SmsMessage[] msgs;
         String strMessage = "";
         String format = bundle.getString("format");
-        // Retrieve the SMS message received.
+        // Obtenemos el sms recibido y lo almacenamos en un array de objetos que obtendremos del extra "pdus" del intent
         Object[] pdus = (Object[]) bundle.get("pdus");
+        //Si el array no es null (se ha recibido un mensaje)
         if (pdus != null) {
-            // Check the Android version.
-            // Fill the msgs array.
+            // Rellenamos el array de sms
             msgs = new SmsMessage[pdus.length];
+            //Recorremos el array de sms y creamos un mensaje a raíz de la pdu en la posición especificada
             for (int i = 0; i < msgs.length; i++) {
+
                 msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
-                // Build the message to show.
+
+                // Obtenemos el texto del mensaje
                 strMessage += "SMS from " + msgs[i].getOriginatingAddress();
                 strMessage += " :" + msgs[i].getMessageBody() + "\n";
-                // Log and display the SMS message.
+
+                // Obtenemos el código de entre todos los caracteres del texto mediante una expresión regular
                 String regex = "(\\d{8}$)";
 
+                //Creamos el patrón para recuperar el trozo de texto deseado
                 Pattern patron = Pattern.compile(regex);
 
+                //Creamos el matcher que nos permitirá encontrar y extraer dicho trozo del texto
                 Matcher matcher = patron.matcher(strMessage);
 
                 try {
+                    //Encontramos el código
                     matcher.find();
 
+                    //Obtenemos una instancia del MainActivity e invocamos al método actualizarEditText(), pasándole
+                    //como parámetro el resultado del matcher.group()
                     MainActivity.devuelveInstancia().actualizarEditText(matcher.group(1));
 
+                    //Pasamos al MainActivity el mensaje recibido para que pueda realizar la comprobación
                     MainActivity.devuelveInstancia().setMensajeCodigo(strMessage);
 
                 }catch(IllegalStateException e)
                 {
+                    //Si el formato del código no sirve se lanza una excepción para que no se cierre la app
                     Toast.makeText(context,"Código no válido", Toast.LENGTH_LONG).show();
                 }
             }
